75dfe977c7ced34e5fda955c5d6dc3e9b80205ff
Ibrahim Orra <55318588+ibrahimorra@users.noreply.github.com>
2021-05-27 19:51:32 -0300

    Update controller.c

--- controller.c
+++ controller.c
@@ -12,7 +12,7 @@
 #define LEDS 10
 #define nr_caixas 9
 #define TIME_STEP 64
-#define MAX_SPEED 6.28
+#define MAX_SPEED 10
 
 static const char* pos_led[LEDS] = { "led0", "led1", "led2", "led3", "led4","led5", "led6", "led7", "led8", "led9" };
 const char sensores_robo[8][4] = { "ps0", "ps1", "ps2", "ps3","ps4", "ps5", "ps6", "ps7" };
@@ -21,6 +21,8 @@ const char node_caixas[9][12] = { "caixa1", "caixa2", "caixa3", "caixa4","caixa5
 
 // Mapa dos Objetos
 double** mapa(int linha, int coluna);
+// Checa se algum objeto foi detectado
+void objetoDetectado(bool detectado, WbFieldRef* caixa_def, double** caixa_posicao, WbDeviceTag* leds);
 // Movimentação do Robo
 static void movimento();
 // Aguardar para o proximo movimento
@@ -39,7 +41,7 @@ static int obterMovimento();
 void sensorAproximidade(WbDeviceTag* sensores_proximidade);
 
 // Atualizar a Posição da Caixa em relação ao Robo
-void attPosCaixa(double* boxInitialPosition, double* boxCurrentPosition);
+void attPosCaixa(double* boxInitialPosition, const double* boxCurrentPosition);
 // node Caixa
 void defCaixa(WbNodeRef* caixas, WbFieldRef* caixa_def);
 // node Caixa Posição em relação ao Robo
@@ -61,21 +63,15 @@ void defineLed(WbDeviceTag* leds);
 int main(int argc, char **argv) {
   wb_robot_init();
 
+  double **caixa_posicao = mapa(9,3);
   sensor_obstaculos* sensor_obstaculos = malloc(sizeof(sensor_obstaculos));
-
+  
+  WbDeviceTag leds[LEDS];
+  WbDeviceTag sensores_proximidade[8];
   WbDeviceTag esquerda_motor = wb_robot_get_device("left wheel motor");
   WbDeviceTag direita_motor = wb_robot_get_device("right wheel motor");
-  WbDeviceTag sensores_proximidade[8];
-
-  WbDeviceTag leds[LEDS];
-  bool leds_robo[LEDS];
-
   WbNodeRef caixas[9];
   WbFieldRef caixa_def[9];
-  double **caixa_posicao = mapa(9,3);
-  
-  WbNodeRef node_robo = wb_supervisor_node_get_from_def("node");
-  WbFieldRef field_robo = wb_supervisor_node_get_field(node_robo, "translation");
 
   sensorAproximidade(sensores_proximidade);
   iniciarRobo(&esquerda_motor, &direita_motor);
@@ -104,6 +100,25 @@ double** mapa(int linha, int coluna){
   return m;
 }
 
+void objetoDetectado(bool detectado, WbFieldRef* caixa_def, double** caixa_posicao, WbDeviceTag* leds){
+  bool objeto_movel = false;
+  if (detectado){
+    for(int i = 0; i < nr_caixas; i++){
+        const double* posicao_box= wb_supervisor_field_get_sf_vec3f(caixa_def[i]);
+        for(int j = 0; j < 3; j++){
+          if(posicao_box[j] != caixa_posicao[i][j]){        
+            objeto_movel = true;
+            attPosCaixa(caixa_posicao[i], posicao_box);
+            break;
+          }  
+        } 
+    }
+    
+    if(objeto_movel)
+      ativarLed(leds);  
+  }     
+}
+
 static void movimento() {
     if (wb_robot_step(obterMovimento()) == -1) {
         wb_robot_cleanup();
@@ -119,18 +134,15 @@ static void aguardar(double tempo) {
 }
 
 void velRobo(sensor_obstaculos* obstaculo, WbDeviceTag* motor_esquerdo, WbDeviceTag* motor_direito){
-  
-  double vel_esquerda  = 0.5 * MAX_SPEED;
-  double vel_direita = 0.5 * MAX_SPEED;
+  double vel_esquerda  = MAX_SPEED;
+  double vel_direita = MAX_SPEED;
   
    if (obstaculo->esquerda) {
-      vel_esquerda  += 0.5 * MAX_SPEED;
-      vel_direita -= 0.5 * MAX_SPEED;
-   }
-  
-   else if (obstaculo->direita) {
-      vel_esquerda  -= 0.5 * MAX_SPEED;
-      vel_direita += 0.5 * MAX_SPEED;
+      vel_esquerda  += MAX_SPEED;
+      vel_direita -= MAX_SPEED;
+   }else if (obstaculo->direita) {
+      vel_esquerda  -= MAX_SPEED;
+      vel_direita += MAX_SPEED;
    }
    
    wb_motor_set_velocity(*motor_esquerdo, vel_esquerda);
@@ -159,7 +171,7 @@ void sensorAproximidade(WbDeviceTag* sensores_proximidade){
   }
 }
 
-void attPosCaixa(double* boxInitialPosition, double* boxCurrentPosition){
+void attPosCaixa(double* boxInitialPosition, const double* boxCurrentPosition){
   for(int i = 0; i < 3; i++){ 
      boxInitialPosition[i] = boxCurrentPosition[i];  
   }
@@ -174,7 +186,7 @@ void defCaixa(WbNodeRef* caixas, WbFieldRef* caixa_def){
 
 void defCaixaPos(WbFieldRef* caixa_def, double** caixa_posicao){
   for(int i = 0; i < 9; i++){
-    double* posicao_box = wb_supervisor_field_get_sf_vec3f(caixa_def[i]);
+    const double* posicao_box = wb_supervisor_field_get_sf_vec3f(caixa_def[i]);
     for(int j = 0; j < 3; j++)
       caixa_posicao[i][j] = posicao_box[j];
   }
@@ -182,8 +194,7 @@ void defCaixaPos(WbFieldRef* caixa_def, double** caixa_posicao){
 
 void verificarColisao(WbDeviceTag* sensores_proximidade, WbFieldRef* caixa_def, double** caixa_posicao, WbDeviceTag* leds){
   bool objeto_detectado = false;
-  bool objeto_movel = false;
-  
+
   for(int i = 0; i < nr_caixas-1; i++){
     float sensor = wb_distance_sensor_get_value(sensores_proximidade[i]);
     if(sensor > 100){
@@ -192,39 +203,19 @@ void verificarColisao(WbDeviceTag* sensores_proximidade, WbFieldRef* caixa_def, 
     }
   }
   
-  if (objeto_detectado){
-     for(int i = 0; i < nr_caixas; i++){
-        double* posicaoBox = wb_supervisor_field_get_sf_vec3f(caixa_def[i]);
-        for(int j = 0; j < 3; j++){
-          if(posicaoBox[j] != caixa_posicao[i][j]){        
-            objeto_movel = true;
-            attPosCaixa(caixa_posicao[i],posicaoBox);
-            break;
-          }  
-        } 
-     }
-     
-     if(objeto_movel)
-       ativarLed(leds);        
-  }
+  objetoDetectado(objeto_detectado, caixa_def, caixa_posicao, leds);
 }
 
 void verificarObstaculos(WbDeviceTag* sensores_proximidade, sensor_obstaculos* sensor_obstaculos){
   double valor_sensores_proximidade[8];
+  
   for (int i = 0; i < nr_caixas-1 ; i++){
       valor_sensores_proximidade[i] = wb_distance_sensor_get_value(sensores_proximidade[i]);
   }
-  
-  bool direita =
-      valor_sensores_proximidade[0] > 80.0 ||
-      valor_sensores_proximidade[1] > 80.0 ||
-      valor_sensores_proximidade[2] > 80.0;
-      
-  bool esquerda =
-      valor_sensores_proximidade[5] > 80.0 ||
-      valor_sensores_proximidade[6] > 80.0 ||
-      valor_sensores_proximidade[7] > 80.0;
-      
+
+  bool direita = valor_sensores_proximidade[0] > 80.0 || valor_sensores_proximidade[1] > 80.0 || valor_sensores_proximidade[2] > 80.0;
+  bool esquerda = valor_sensores_proximidade[5] > 80.0 || valor_sensores_proximidade[6] > 80.0 || valor_sensores_proximidade[7] > 80.0;
+     
   sensor_obstaculos->direita = direita;
   sensor_obstaculos->esquerda = esquerda;
        
